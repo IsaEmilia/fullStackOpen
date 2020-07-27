a1a070eb660c26eb299ec845f7171c7541d028ae
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-07-27 14:07:58 +0300

    Make a counter for total exercises,
    Just for fun.

--- part1/src/Total.js
+++ part1/src/Total.js
@@ -1,12 +1,30 @@
-import React from "react"
+import React, {useState} from "react"
 
 const Total = (props) => {
-   
-  console.log(props.parts)
+
+  const sum = (props.parts[0].exercises)+(props.parts[1].exercises)+(props.parts[2].exercises)
+
+
+  const [ counter, setCounter ] = useState(sum)
+
+
+  const increase = () => setCounter(counter + 1)
+  const decrease = () => setCounter(counter - 1)
+  const reset = () => setCounter(sum)
+
+  const randomize = () => {
+    const maxNumber = 100;
+    const randomNumber = setCounter(Math.floor((Math.random() * maxNumber) + 1));
+  }
+  
 
   return (
       <div>
-        <p>Number of exercises: {(props.parts[0].exercises)+(props.parts[1].exercises)+(props.parts[2].exercises)}</p>
+        <p>Number of exercises: {counter}</p>
+        <button onClick={increase}>+</button>
+        <button onClick={decrease}>-</button>
+        <button onClick={randomize}>?</button>
+        <button onClick={reset}>reset</button>
           <hr></hr>
       </div>
   )
