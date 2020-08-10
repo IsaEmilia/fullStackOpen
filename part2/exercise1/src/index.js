c563fb72a6ebfd36a1b433cb588d444cc9c47878
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-08-10 11:03:38 +0300

    Phonebook exercise

--- part2/exercise1/src/index.js
+++ part2/exercise1/src/index.js
@@ -3,20 +3,45 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { content: 'Arto Hellas', id: 0 }
   ]) 
-  const [ newName, setNewName ] = useState('')
+  const [ newName, setNewName ] = useState([''])
 
+
+  const addPerson = (event) => {
+     event.preventDefault()
+     const nameObject = {
+       content: newName,
+       id: persons.length + 1,
+     }
+
+     setPersons(persons.concat(nameObject))
+     setNewName("")
+  }
   
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
-          name: <input />
-          <button type="submit">add</button>
+
+      <form onSubmit={addPerson}>
+        name:  
+        <input value={newName} 
+               onChange={handleNameChange}/>
+        <button type="submit">add</button>
       </form>
+      
       <h2>Numbers</h2>
+      
+      <ul>
+        {persons.map(name => 
+          <li key={name.id} >{name.content}</li>
+        )} 
+      </ul>
     </div>
   )
 }
