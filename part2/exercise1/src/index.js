7004f2e83dcfe6d2898ecc82e27898aeef80c3ad
Isa Hänninen <55230101+IsaEmilia@users.noreply.github.com>
2020-08-31 11:55:26 +0300

    Try adding features to phone book.
    Prevent adding already existing names. Doesn't work.

--- part2/exercise1/src/index.js
+++ part2/exercise1/src/index.js
@@ -3,25 +3,63 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { content: 'Arto Hellas', id: 0 }
+    { content: 'Name Example', number: '123 456 789', id: 0 }
   ]) 
   const [ newName, setNewName ] = useState([''])
+  const [ newNumber, setNewNumber] = useState([''])
 
+
+  const NameList = () => {
+    return(
+      <div>
+      <ul>
+        {persons.map(name => 
+          <li key={name.id} >
+            {name.content} 
+            <br></br> 
+            {name.number}
+          </li> 
+        )} 
+      </ul> 
+      </div>
+    )
+  } 
 
   const addPerson = (event) => {
-     event.preventDefault()
+    event.preventDefault()
      const nameObject = {
        content: newName,
+       number: newNumber,
        id: persons.length + 1,
-     }
+    }
 
-     setPersons(persons.concat(nameObject))
-     setNewName("")
+    if (newName === "r") {
+      alert(`${newName} is already added`)
+      return(
+        null
+      )
+    } if(newName === ""){
+      return(
+        null
+      )
+    }if(newNumber === ""){
+      alert('Phone number is missing')
+      return(
+        null
+      )
+    }
+    setPersons(persons.concat(nameObject))
+    setNewName("")
+    setNewNumber("")
   }
   
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -29,19 +67,23 @@ const App = () => {
       <h2>Phonebook</h2>
 
       <form onSubmit={addPerson}>
-        name:  
+        name:  <br></br>
         <input value={newName} 
                onChange={handleNameChange}/>
+        <br></br>
+        number:  <br></br>
+        <input value={newNumber} 
+               onChange={handleNumberChange}/>
+        <br></br>
         <button type="submit">add</button>
       </form>
       
       <h2>Numbers</h2>
+
+  <div>
+    <NameList />
+  </div>
       
-      <ul>
-        {persons.map(name => 
-          <li key={name.id} >{name.content}</li>
-        )} 
-      </ul>
     </div>
   )
 }
